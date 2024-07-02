6cce124002d441519cfa45f0a08fba6a8608926c
Mahmoud Anwer <mahmoudanwer071@gmail.com>
2024-07-02 14:44:36 +0300

    update data route

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Depends, UploadFile, status
 from fastapi.responses import JSONResponse
 from helpers.config import get_settings, Settings
-from controllers import DataController, ProjectController
+from controllers import DataController, ProjectController, ProcessController
 from models import ResponseSignal
+from .schemes.data import ProcessRequest
 import aiofiles
-import os
 import logging
+
 
 logger = logging.getLogger('uvicorn.error')
 
@@ -50,3 +51,28 @@ async def upload_data(project_id: str, file: UploadFile,
                 "file_id": file_id
             }
         )
+
+@data_router.post("/process/{project_id}")
+async def process_endpoint(project_id: str, process_request: ProcessRequest):
+    file_id = process_request.file_id
+    chunk_size = process_request.chunk_size
+    overlap_size = process_request.overlap_size
+
+    process_controller = ProcessController(project_id=project_id)
+    file_content = process_controller.get_file_content(file_id=file_id)
+    
+    file_chunks = process_controller.process_file_content(
+        file_content=file_content,
+        file_id=file_id,
+        chunk_size=chunk_size,
+        overlap_size=overlap_size    
+    )
+
+    if file_chunks is None or len(file_chunks) == 0:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.PROCESSING_FAILD.value
+            }
+        )
+    return file_chunks
